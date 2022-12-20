5676ba9e444c1496f509d791cd2cac26806e7330
Amarsanaa <amarsanaa065@gmail.com>
2022-12-20 17:17:48 +0800

    ref ashiglasan jijig jishee

--- src/App.js
+++ src/App.js
@@ -1,24 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect, useRef } from "react";
+
+
 
 function App() {
+  const [name, setName] = useState(''); 
+  const inputRef = useRef()
+  
+  function focus (){
+    inputRef.current.focus()
+    inputRef.current.value = 'Some value'
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <input ref={inputRef} value={name} onChange={e => setName(e.target.value)}/>
+      <div>my name is {name}</div>
+      <button onClick={focus}>Focus</button> 
+    </>
   );
 }
 
